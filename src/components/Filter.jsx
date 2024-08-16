abf296b730faf694fd5e15d9eba5c9a1596f3fc9
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-08-16 15:22:32 +0200

    Created filter component.

---
+++ src/components/Filter.jsx
@@ -0,0 +1,6 @@
+import { IconButton } from "@chakra-ui/react";
+import { FaFilter } from "react-icons/fa6";
+
+export const FilterEvents = () => {
+  <IconButton colorScheme="green" icon={<FaFilter />} />;
+};
