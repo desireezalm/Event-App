9b8753a8c74b221aefa69d25d2aa78238c8c2be1
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-08-23 16:51:15 +0200

    Moved code for Event Page Card to Event Card component.

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { Center } from "@chakra-ui/react";
+import { useLoaderData } from "react-router-dom";
+import { EventCardDetails } from "../components/util/EventCard";
+
+export const loader = async ({ params }) => {
+  const users = await fetch("http://localhost:3000/users");
+  const eventItem = await fetch(
+    `http://localhost:3000/events/${params.eventId}`
+  );
+  const categories = await fetch("http://localhost:3000/categories");
+
+  return {
+    users: await users.json(),
+    eventItem: await eventItem.json(),
+    categories: await categories.json(),
+  };
+};
 
 export const EventPage = () => {
-  return <Heading>Event</Heading>;
+  const { eventItem, users, categories } = useLoaderData();
+  return (
+    <Center className="event-page">
+      <EventCardDetails
+        categories={categories}
+        users={users}
+        key={eventItem.id}
+        item={eventItem}
+      />
+    </Center>
+  );
 };
