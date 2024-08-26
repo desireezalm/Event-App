ce81b3b4f69a927a79952597b03796f5a3d4aeb0
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-08-26 13:39:50 +0200

    Moved filter handler to Events Page.

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,13 @@
 import { Icon, Select, Flex } from "@chakra-ui/react";
 import { FaFilter } from "react-icons/fa6";
 
-export const FilterEvents = () => {
+export const FilterEvents = ({ handleFilter, ...props }) => {
+  /*
+  const handleFilter = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  */
+
   return (
     <Flex
       className="search-events"
@@ -12,30 +18,36 @@ export const FilterEvents = () => {
       alignItems="center"
       justifyContent="flex-start"
     >
-      <Icon
-        color="green.400"
-        height={{ base: "1.2rem", md: "1.4rem" }}
-        width={{ base: "1.2rem", md: "1.4rem" }}
-        marginRight="1rem"
-        as={FaFilter}
-      />
+      <label htmlFor="category-select">
+        <Icon
+          color="green.400"
+          height={{ base: "1.2rem", md: "1.4rem" }}
+          width={{ base: "1.2rem", md: "1.4rem" }}
+          marginRight="1rem"
+          as={FaFilter}
+        />
+      </label>
       <Select
+        name="category"
+        id="category-select"
         variant="outline"
         placeholder="Filter events"
-        color="green.400"
-        _placeholder={{ opacity: 0, color: "green.400" }}
-        borderColor="green.400"
-        _hover={{ borderColor: "green.300" }}
-        focusBorderColor="green.200"
+        _placeholder={{ opacity: 1, color: "green.400" }}
         fontSize={{ base: "1rem", sm: "1.2rem" }}
         width={{ base: "10rem", md: "14rem" }}
         size={{ base: "md", md: "lg" }}
         borderRadius="10px"
+        color="green.400"
+        borderColor="green.400"
+        focusBorderColor="green.200"
+        _hover={{ borderColor: "green.300" }}
+        onChange={handleFilter}
+        {...props}
       >
-        <option value="exercise">Exercise</option>
-        <option value="cuisine">Cuisine</option>
-        <option value="relaxation">Relaxation</option>
-        <option value="entertainment">Entertainment</option>
+        <option value="1">Exercise</option>
+        <option value="2">Cuisine</option>
+        <option value="3">Relaxation</option>
+        <option value="4">Entertainment</option>
       </Select>
     </Flex>
   );
