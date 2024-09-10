12f4bc6dd83292609414dbc774f84b408388dbae
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2024-09-10 15:24:30 +0200

    Minor changes to styling of LoadingState component

--- src/components/util/LoadingState.jsx
+++ src/components/util/LoadingState.jsx
@@ -2,8 +2,16 @@ import { Center, Heading, Spinner } from "@chakra-ui/react";
 
 export const LoadingState = () => {
   return (
-    <Center>
-      <Heading as={"h2"} fontSize={{ base: "1rem", md: "2.2rem" }}>
+    <Center
+      padding={{ base: "1rem", md: "2.5rem" }}
+      margin="5rem auto"
+      bgColor="green.500"
+      borderRadius="0.8rem"
+      width={{ base: "80%" }}
+      color="white"
+      flexDirection="column"
+    >
+      <Heading as={"h2"} fontSize={{ base: "1.5rem", md: "2.2rem" }}>
         Page is loading
       </Heading>
       <Spinner
@@ -13,7 +21,7 @@ export const LoadingState = () => {
         speed="0.80s"
         thickness="0.3rem"
         padding={{ base: "0.5rem", md: "1rem" }}
-        margin={{ base: "0 0.7rem", md: "0 1rem" }}
+        margin={{ base: "1rem 0.5rem" }}
       />
     </Center>
   );
